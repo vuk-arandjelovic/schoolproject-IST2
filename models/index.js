2660e6d123c9ebf12b05e695e4f2df3a45826ea2
vuk-arandjelovic <arandjelovicvuk@gmail.com>
2023-06-11 05:47:48 +0200

    Added console logs for API calls, fixed model exports

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-const Film = require("./film")
-const CinemaHall = require("./cinemahall")
-const Reservation = require("./reservation")
-const Projection = require("./projection")
+const {Film} = require("./film")
+const {CinemaHall} = require("./cinemahall")
+const {Reservation} = require("./reservation")
+const {Projection} = require("./projection")
 
 module.exports = {
   Film,
--- routes/films.js
+++ routes/films.js
@@ -5,12 +5,14 @@ const { Film } = require("../models");
 // returns all films
 router.get('/', async (req, res) => {
   const films = await Film.find();
+  console.log("Get films: \n")
   res.json(films);
 });
 
 // returns one films by id
 router.get('/:id', async (req, res) => {
   const film = await Film.findById(req.params.id);
+  console.log("Get film by id: \n"+film)
   res.json(film);
 });
 
@@ -19,6 +21,7 @@ router.post('/', async (req, res) => {
   const film = new Film(req.body);
   try {
     const savedFilm = await film.save();
+    console.log("Added movie to database: \n"+savedFilm)
     res.status(201).json(savedFilm);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -29,6 +32,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   try {
     const updatedFilm = await Film.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    console.log("Updated movie from database: \n"+updatedFilm)
     res.json(updatedFilm);
   } catch (error) {
     res.status(400).json({ error: error.message });
@@ -39,6 +43,7 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   try {
     const deletedFilm = await Film.findByIdAndDelete(req.params.id);
+    console.log("Deleted movie from database: \n"+deletedFilm)
     res.json(deletedFilm);
   } catch (error) {
     res.status(400).json({ error: error.message });
